d40c1298fee2af1e115c3a82a60655c6b0b13625
S-Samoylov <s02150286@stud.cs.msu.ru>
2021-05-14 08:34:48 +0300

    Cleaning 2

--- Kirichenko/find_where_all_4/gen_all.sh
+++
@@ -1,8 +0,0 @@
-i=1
-while read p; do
-  echo "$i vector"
-  echo "$i " >> result_performed_48_shadows_random_vecs_4_100.txt
-  echo "$p" | python kir_one_shadow_performed_for_some_shadows.py >> result_performed_48_shadows_random_vecs_4_100.txt
-  i=$(( $i + 1 ))
-done < random_vecs_4_100.txt
-echo 'n = 4 ended'
--- Kirichenko/test_kir_one_vecs.sh
+++
@@ -1,8 +0,0 @@
-i=1
-while read p; do
-  echo "$i vector"
-  echo "$i " >> result_performed_random_vecs_9_100.txt
-  echo "$p" | python kir_one_shadow_performed.py >> result_performed_random_vecs_9_100.txt
-  i=$(( $i + 1 ))
-done < random_vecs_9_100.txt
-
